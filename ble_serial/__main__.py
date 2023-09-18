6aa2a967c5f27b6ca814785747c3ea48bd3592e8
Branimir Ri\v{c}ko <rickobranimir@gmail.com>
2023-09-18 22:27:38 +0200

    Stuff for logging packet loss.

--- ble_serial/__main__.py
+++ ble_serial/__main__.py
@@ -11,17 +11,33 @@ addr = ("127.0.0.1", 42069)
 p = DBParser()
 f = open("out.csv", "w+")
 
+class stataus:
+    def __init__(self) -> None:
+        self.last_rcevid: int = 0
+        self.lps: int = 0
+        self.lps_r: int = 0
+
+st = stataus()
+
 def receive_callback(handle: BleakGATTCharacteristic, value: bytes):
     print("Received:", value)
     p.add_data(handle.handle, value)
     d = p.pop_data()
     while d != None:
         print(d)
-        f.write(f"{d[1]},")
+        #f.write(f"{d[1]},")
+        if d[0] == 1307:
+            if d[1] > st.last_rcevid + 1:
+                st.lps += int(d[1]) - int(st.last_rcevid) - int(1)
+                st.lps_r += int(d[1]) - int(st.last_rcevid) - int(1)
+            st.last_rcevid = int(d[1])
         client_socket.sendto(f"{d[1]};{d[0]} ".encode(), addr)
         d = p.pop_data()
-    f.write(f"\n")
-    f.flush()
+    #f.write(f"{st.lps}\n")
+    client_socket.sendto(f"{st.lps};10 {st.lps_r};11 ".encode(), addr)
+    #print(f"LOST PACKETS: {st.lps}")
+
+    #f.flush()
 
 
 # nc -ulkp 42069 | rlplot
@@ -33,6 +49,7 @@ async def hello_sender(ble: BLE_interface):
         ble.queue_send(b"Hello world\n")
 
 async def main():
+    st.lps_r = 0
     try:
         READ_UUID = ['00009a66-0000-1000-8000-00805f9b34fb']
 
